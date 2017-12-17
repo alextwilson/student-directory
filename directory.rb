af17ecbf7f835fe2736689f2cc029b471efdc0c8
Alex Wilson <github@alextwilson.co.uk>
2017-12-17 22:36:58 +0000

    Saving and loading now uses a do...end block

--- directory.rb
+++ directory.rb
@@ -107,23 +107,23 @@ def print_short_names
 end
 
 def save_students(filename)
-  file = File.open(filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
   puts "Students saved to #{filename}"
 end
 
 def load_students(filename)
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    populate_students(name, cohort)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+      populate_students(name, cohort)
+    end
   end
-  file.close
   puts "Students loaded from #{filename}"
 end
 
