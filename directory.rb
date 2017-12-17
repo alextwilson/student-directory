1d8efc037f7d6fba56d6cc6c363682319c0b1f98
Alex Wilson <github@alextwilson.co.uk>
2017-12-17 07:50:55 +0000

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,14 +1,24 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
+  while true do
+    if name.empty?
+      break
+    end
+    puts "Please enter the cohort of the student"
+    cohort = gets.chomp
+    if cohort == ""
+      cohort = "november"
+    end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-  students
+  @students = students
 end
 
 def print_header
@@ -37,9 +47,35 @@ def print_begins_with
   end
 end
 
-@students = input_students
-print_header
-print(@students)
-print_footer(@students)
-print_begins_with
+def print_short_names
+  puts "Here are the students with short names:"
+  @students.each do |student|
+    if student[:name].length <= 12
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def interactive_menu
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      input_students
+    when "2"
+      print_header
+      print(@students)
+      print_footer(@students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try another option"
+    end
+  end
+end
+
+interactive_menu
 
