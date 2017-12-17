c527ccb6208a2042506f7865fe79ebf5397051ff
Alex Wilson <github@alextwilson.co.uk>
2017-12-17 09:15:04 +0000

    Refactoring

--- directory.rb
+++ directory.rb
@@ -3,7 +3,6 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while true do
     if name.empty?
@@ -14,11 +13,10 @@ def input_students
     if cohort == ""
       cohort = "november"
     end
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  @students = students
 end
 
 def print_header
@@ -26,14 +24,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def print_begins_with
@@ -56,24 +54,35 @@ def print_short_names
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try another option"
+  end
+end
+
 def interactive_menu
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      input_students
-    when "2"
-      print_header
-      print(@students)
-      print_footer(@students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try another option"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
